cdba6c354b598aca565d19858e14ca68b6431132
Gabriel-DAloia <g-f-d-m@hotmail.com>
2025-01-25 00:15:08 +0000

    generador de  dominios

--- src/app.js
+++ src/app.js
@@ -1,4 +1,14 @@
 let pronoun = ["the", "our"];
 let adj = ["great", "big"];
 let noun = ["jogger", "racoon"];
-let NewArr;
+let NewArr = [];
+
+for (let p of pronoun) {
+  for (let a of adj) {
+    for (let n of noun) {
+      NewArr.push(p + a + n + ".com");
+    }
+  }
+}
+
+console.log(NewArr);
